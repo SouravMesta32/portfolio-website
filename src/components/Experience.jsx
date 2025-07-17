3ac8aa1de5774c34b006bedcb6c3f21648d03568
sourav <souravmesta32@gmail.com>
2025-07-17 05:50:58 +0530

    added framer motion to the experience and contact section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,21 +1,25 @@
 import { EXPERIENCES } from "../constants"
+import { motion } from "framer-motion"
 
 
 export const Experience = () => {
   return (
     <div className="pb-4">
-        <h2 className="my-20 text-center text-4xl">
+        <motion.h2 className="my-20 text-center text-4xl" whileInView={{opactiy:1 , y:0}} initial={{opacity:0, y:-100}} transition={{duration:0.5}}>
             Experience
-        </h2>
+        </motion.h2>
         <div>
             {EXPERIENCES.map((exp ,index)=>(
-                <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-                    <div className="w-full lg:w-1/4">
+                <div key={index} className="mb-8 flex flex-wrap lg:justify-center" 
+                    whileInView={{opacity:1,x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}}>
+                    <motion.div className="w-full lg:w-1/4">
                         <p className="mb-2 text-sm text-stone-400 ">
                             {exp.year}
                         </p>
-                    </div>
-                    <div className="w-full w-max-xl lg:w-3/4">
+                    </motion.div>
+                    <motion.div 
+                        whileInView={{opacity:1 ,x:0}} initial={{opacity:0, x:100}} transition={{duration:1}}
+                        className="w-full w-max-xl lg:w-3/4">
                         <h3 className="mb-2 font-semibold">
                             {exp.role} -(" ")
                             <span className="text-sm text-stone-500">
@@ -30,7 +34,7 @@ export const Experience = () => {
                                 ))}
                             </p>
                         </h3>
-                    </div>
+                    </motion.div>
                 </div>
             ))}
         </div>
